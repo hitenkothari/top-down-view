e36aee99f16b5835e2fc7af59066cb4559ff55f3
Hiten Kothari <hitenkothari08@gmail.com>
2023-11-25 17:23:14 -0500

    portrait mode

--- topdown_realtime_h.py
+++ topdown_realtime_h.py
@@ -4,10 +4,19 @@ import time
 
 sharp_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
 
+isClosed = True
+ 
+# Blue color in BGR
+color = (255, 0, 0)
+ 
+# Line thickness of 2 px
+thickness = 2
+
 #trapezoid points, have to be pakka finalized
 # trapezoid_pts = np.array([[1320 ,1680], [2550,1680], [2880,2120], [900,2120]], dtype='float32')
 # trapezoid_pts = np.array([[490 ,2480], [1740,2480], [2090,2910], [120,2910]], dtype='float32') #for test_23_11/tab_portrait #this works good for cards tab is getting chopped a bit
-trapezoid_pts = np.array([[490 ,2690], [1660,2690], [2030,3350], [70,3350]], dtype='float32') #for test_25_11/quicktest zoomed #this works good for cards tab is getting chopped a bit
+trapezoid_pts = np.array([[490 ,2690], [1660,2690], [2030,3350], [70,3350]], dtype='float32') # for portrait mode on 2511 4K
+# trapezoid_pts = np.array([[740 ,820], [1180,820], [1380,1080], [570,1080]], dtype='float32') # for landscape mode on 2511 UHD
 
 #size of window//can be experimented
 rectangle_pts = np.array([[0,0],[1920,0],[1920,1080],[0,1080]],dtype='float32')
@@ -23,10 +32,14 @@ while True:
     _, frame = capture.read()
     transformed_image = cv2.warpPerspective(frame, perspective_matrix, (1920, 1080)) 
     transformed_image = cv2.rotate(transformed_image,cv2.ROTATE_180)
-    transformed_image = cv2.filter2D(transformed_image, -1, sharp_kernel) #sharpening
-    transformed_image = cv2.resize(transformed_image,(960,540))
+    transformed_image_s = cv2.filter2D(transformed_image, -1, sharp_kernel) #sharpening
+    # transformed_image = cv2.resize(transformed_image,(960,540))
+    transformed_image_s = cv2.resize(transformed_image_s,(960,540))
+    frame = cv2.polylines(frame, np.int32([trapezoid_pts]), isClosed, color, thickness)
     cv2.imshow("OG",frame)
-    cv2.imshow("TDV",transformed_image)
+    # cv2.imshow("TDV",transformed_image)
+    cv2.imshow("TDVSHarpen",transformed_image_s)
+
 
     if cv2.waitKey(1) == ord("x"): #press x to exit the capture window and take a photo image saved as test.png (will be replaced for each capture)
         cv2.imwrite("test2511.png",frame)
